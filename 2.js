68e3dde7094d593d251916e8247a138a817332ca
God-Hacker-23 <123602833+God-Hacker-23@users.noreply.github.com>
2023-04-29 13:16:33 -0400

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,13 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+
+
+<><p>
+    write about visual studio and how to set it up.
+</p><p>
+        Write about console screen and what it is used for.
+    </p></>
+   var add = a+b;
+   console.log(add); 
